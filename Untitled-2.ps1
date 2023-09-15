2610c52948c9211561c6f4089bc7eaf56107a115
Iamneo <techadmin@iamneo.ai>
2023-09-15 19:09:08 +0000

    Backup Commit

--- Untitled-2.ps1
+++ Untitled-2.ps1
@@ -1,11 +1,14 @@
-$filePath = $env:USERPROFILE
+$folderPath = [System.IO.Path]::Combine($env:USERPROFILE, "Documents")
 
-$fileContent = "Tarun Sharma"
+$fileName = "NewFile.txt"
 
-$fileContent | Set-Content -Path $filePath
-
-if (Test-Path -Path $filePath -PathType Leaf) {
-    Write-Host "File created at $filePath"
-} else {
-    Write-Host "File creation failed."
+if (-not (Test-Path -Path $folderPath -PathType Container)) {
+    New-Item -Path $folderPath -ItemType Directory
+    Write-Host "Folder created: $folderPath"
 }
+
+$filePath = Join-Path -Path $folderPath -ChildPath $fileName
+
+$fileContent = @"
+I am loving Ignite Program
+"@
