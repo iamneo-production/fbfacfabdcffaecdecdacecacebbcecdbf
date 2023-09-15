6508e0564287629eb479362e9e2229831dd05503
Iamneo <techadmin@iamneo.ai>
2023-09-15 19:44:08 +0000

    Backup Commit

--- Untitled-5.ps1
+++ Untitled-5.ps1
@@ -7,4 +7,8 @@ $subfolder1
 $subfolder2 = New-Item -Path $newdir\subfolder2 -ItemType Directory
 $subfolder2
 
-$subfolder1files = New-Item -Path $subfolder1
+$subfolder1files = New-Item -Path $subfolder1\Test1.txt,$subfolder1\Test2.txt,$subfolder1\Test3.txt
+$subfolder1files
+
+$subfolder2files = New-Item -Path $subfolder2\Test4.txt,$subfolder2\Test5.txt,$subfolder2\Test6.txt
+$subfolder2files
