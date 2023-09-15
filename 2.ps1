af07d5a60a8d74f202ab26393adc18833e3018d4
Iamneo <techadmin@iamneo.ai>
2023-09-15 14:29:00 +0000

    Backup Commit

--- 2.ps1
+++ 2.ps1
@@ -1,1 +1,7 @@
-$filePath = ""
+$filePath = "Tarun(D:)\"
+
+$fileContent = "This is the text content of the file.'r'nYou can add more lines as needed."
+
+$fileContent | Out-File -FilePath $filePath -Encoding UTF8
+
+Write-Host "File '$filePath' created and text content written to it."
