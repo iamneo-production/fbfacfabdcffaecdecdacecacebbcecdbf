0b180e09b19dfdf58566dad7c138f542f3b609e2
Iamneo <techadmin@iamneo.ai>
2023-09-15 19:29:08 +0000

    Backup Commit

---
+++ Untitled-4.ps1
@@ -0,0 +1,9 @@
+$number1 = Read-Host "Enter the first number:"
+$number2 = Read-Host "Enter the second number:"
+
+$number1 = [int]$number1
+$number2 = [int]$number2
+
+$total = $number1 + $number2
+
+if ($t)
